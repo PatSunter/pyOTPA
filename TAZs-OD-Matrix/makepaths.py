5adcb862570c344e60e3fbb51965722546f5c669
Pat Sunter <patdevelop@gmail.com>
2014-05-08 18:06:10 +1000

    Just changed the makepaths.py so it doesn't automatically run the makepaths function if you're importing from another Python script.

--- TAZs-OD-Matrix/makepaths.py
+++ TAZs-OD-Matrix/makepaths.py
@@ -3,12 +3,16 @@
 import csv
 import os
 
-tfile = open('/Users/Shared/SoftwareDev/UrbanModelling-GIS/OpenTripPlanner/BatchResults/tazs-current/taz_locs.csv', 'rb')
+def make_paths(template_filename):
+    tfile = open(template_filename, 'rb')
 
-treader = csv.reader(tfile, delimiter=',', quotechar="'")
-for ii, row in enumerate(treader):
-    if ii == 0: continue
-    else:
-        dirname = '%d_%s' % (ii-1, row[0])
-        print dirname
-        os.makedirs(dirname)
+    treader = csv.reader(tfile, delimiter=',', quotechar="'")
+    for ii, row in enumerate(treader):
+        if ii == 0: continue
+        else:
+            dirname = '%d_%s' % (ii-1, row[0])
+            print dirname
+            os.makedirs(dirname)
+
+if __name__ == "__main__":
+    make_paths('/Users/Shared/SoftwareDev/UrbanModelling-GIS/OpenTripPlanner/BatchResults/tazs-current/taz_locs.csv')
