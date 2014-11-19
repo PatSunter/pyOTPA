a74e672a99819d9145a2c78995e11e71af22156e
Pat Sunter <patdevelop@gmail.com>
2014-11-19 14:31:03 +1100

    New functions to help with creating a set of graphs.

--- Graphs/manage_graphs.py
+++ Graphs/manage_graphs.py
@@ -1,5 +1,7 @@
 
 import os.path
+import operator
+import itertools
 
 GTFS_BEAN = "org.opentripplanner.graph_builder.model.GtfsBundle"
 GTFS_PROP_PATH = "path"
@@ -13,6 +15,13 @@ MODE_ALLOW_BIKES = {
     'bus-mway': 'false'}
 
 GRAPH_JAR='graph-builder.jar'
+
+MODE_PLURALS = {
+    'train': 'trains',
+    'tram': 'trams',
+    'bus': 'buses',
+    'bus-mway': 'mway_buses',
+    }
 
 def create_gtfs_xml_entry(mode, gtfs_type, gtfs_name, agency_id,
             get_gtfs_fname_func):
@@ -83,3 +92,71 @@ def create_graph(graph_config_xml_fname, otp_base):
     os.system(cmdline)
     print "...done."
     return
+ 
+### Funcs for manipulating a group of graphs.
+
+def pprint_all_graph_specs(pp, gspecs):
+    """argument pp is a pretty printer."""
+    for gname, gspec in gspecs.iteritems():
+        print gname
+        pp.pprint(gspec)
+    print ""    
+
+#def process_gtfs_entry_X(mode_spec, mode_ext,
+#        mode_non_orig, mode_upgraded, mode_versions):
+#   Must return a tuple of (graph type, gtfs name, agency id) - all str
+
+def create_graphs(graph_name_base, process_gtfs_entry_func, list_iterator_func,
+        *mode_specs):
+    """process_gtfs_entry_func passed as a func pointer, since it is specific
+    to the type of graphs you're handling.
+    
+    list_iterator_func will be applied to the extensions of the mode_specs.
+    E.g. pass in itertools.product, or itertools.izip.
+    """
+    graph_specs = {}
+    modes_list = map(operator.itemgetter(0), list(mode_specs))
+    # There could legitimately be duplicates in the above, so build a list
+    # with no dups.
+    all_modes = []
+    for mode in modes_list:
+        if mode not in all_modes:
+            all_modes.append(mode)
+    for mode_combo in \
+            list_iterator_func(*map(operator.itemgetter(2), list(mode_specs))):
+        graph_name = graph_name_base + "-add"
+        graph_spec = {}
+        mode_non_orig = {}
+        mode_upgraded = {}
+        mode_versions = {}
+        for mode in all_modes:
+            graph_spec[mode] = []
+            mode_non_orig[mode] = False
+            mode_upgraded[mode] = False
+            mode_versions[mode] = None
+        for mode_i, mode_ext in enumerate(mode_combo):
+            mode = mode_specs[mode_i][0]
+            if mode_ext == None or type(mode_ext) == str:
+                gtfs_entry = process_gtfs_entry_func(mode_specs[mode_i], mode_ext,
+                    mode_non_orig, mode_upgraded, mode_versions)
+                graph_spec[mode].append(gtfs_entry)
+            else:
+                # There are multiple entries for this mode-type in the combo.
+                mode_ext_group = mode_ext
+                for sub_mode_ext in mode_ext_group:
+                    gtfs_entry = process_gtfs_entry_func(mode_specs[mode_i],
+                        sub_mode_ext, mode_non_orig, mode_upgraded,
+                        mode_versions)
+                    graph_spec[mode].append(gtfs_entry)
+        for mode in all_modes:
+            mode_str = ""
+            if mode_non_orig[mode]:
+                if mode_upgraded[mode]:
+                    mode_str = '_upgraded_%s' % MODE_PLURALS[mode]
+                else:
+                    mode_str += '_non_upgraded_%s' % MODE_PLURALS[mode]
+                if mode_versions[mode]:
+                    mode_str += '-' + mode_versions[mode]
+            graph_name += mode_str
+        graph_specs[graph_name] = graph_spec
+    return graph_specs
