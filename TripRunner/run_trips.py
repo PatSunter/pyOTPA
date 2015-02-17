8a0414c85af4dc6c8a4be491abb293ff9c3a2d3b
Pat Sunter <patdevelop@gmail.com>
2015-02-17 17:49:38 +1100

    Added ability to save a bunch of trip itinerary results to file, and re-load for later analysis.
    Need to refine further to handle trips that don't return a valid itinerary
    etc.

--- TripRunner/run_trips.py
+++ TripRunner/run_trips.py
@@ -4,10 +4,12 @@ import urllib
 import urllib2
 import os.path
 import json
+import glob
 from datetime import datetime, date, time
 from osgeo import osr
 
 import trip_analysis
+import Trips_Generator.trips_io
 
 OTP_DATE_FMT = "%Y-%m-%d"
 OTP_TIME_FMT = "%H:%M:%S"
@@ -45,30 +47,6 @@ def run_trip(server_url, routing_params, trip_req_start_date,
     data = response.read()
     return data
 
-def route_trips_and_save_details(server_url, otp_router_id, save_path,
-        save_suffix, trips, date, routing_params): 
-
-    if os.path.exists(save_path) is False: 
-        os.makedirs(save_path)
-
-    for trip_id, trip_tuple in trips.iteritems():
-            print "About to request routes for trip %d,and save results:" \
-                % (trip_id)
-
-            date_mod, time_mod = date_time_tuple
-            url = build_trip_request_url(server_url, routing_params,
-                date_mod, time_mod, origin_lon_lat, dest_lon_lat, 
-                otp_router_id)
-            print url
-            response = urllib2.urlopen(url)
-            data = response.read()
-            f = open(fname, "w")
-            f.write(data)
-            f.close()
-
-            print "DONE!\n"
-    return
-
 def run_single_trip_multi_graphs_print_stats(server_url, routing_params,
         graph_specs, trip_req_start_date, trip_req_start_time,
         origin_lon_lat, dest_lon_lat):    
@@ -78,7 +56,7 @@ def run_single_trip_multi_graphs_print_stats(server_url, routing_params,
     print "\nCalling server to route a trip from %s to %s, leaving "\
         "at %s:" % (origin_lon_lat, dest_lon_lat, trip_req_start_dt)
 
-    for graph_short, graph_full in graph_specs.iteritems():
+    for graph_name, graph_full in graph_specs.iteritems():
         res_str = run_trip(server_url, routing_params,
             trip_req_start_date, trip_req_start_time, origin_lon_lat,
             dest_lon_lat, otp_router_id=graph_full)
@@ -86,36 +64,72 @@ def run_single_trip_multi_graphs_print_stats(server_url, routing_params,
         #import pprint
         #pp = pprint.PrettyPrinter(indent=2)
         #pp.pprint(res)
-        itin = res['plan']['itineraries'][0]
+        itin_json = res['plan']['itineraries'][0]
         print "\nRouting on the %s network/timetable, Requested trip stats:" \
-            % graph_short
+            % graph_name
+        ti = trip_analysis.TripItinerary(itin_json)
         trip_analysis.print_single_trip_stats(origin_lon_lat, dest_lon_lat,
-            trip_req_start_dt, itin)
+            trip_req_start_dt, ti)
     return
 
-def read_then_route_trip_set(trips_shpfilename, trips_start_date,
-        graph_specs):
-    otp_router_srs = osr.SpatialReference()
-    otp_router_srs.ImportFromEPSG(OTP_ROUTER_EPSG)
+def route_trip_set_on_graphs(server_url, routing_params,
+        graph_specs, trip_req_start_date, trips, trips_by_id, output_base_dir):
 
-    trip_id_map, trips = Trips_Generator.trips_io.read_trips_from_shp_file(
-        trips_shpfilename, otp_router_srs)
+    trip_results_by_graph = {}
+    trip_req_start_dts = {}
+    for trip_id, trip in trips_by_id.iteritems():
+        trip_req_start_dts[trip_id] = datetime.combine(trip_req_start_date,
+            trip[2])
 
-    for graph_short, graph_full in graph_specs.iteritems():
-        for trip in trips:
-            trip_req_start_dt = datetime.combine(trip_req_start_date,
-                trip[2])
-            res_str = run_trip(SERVER_URL, ROUTING_PARAMS,
+    for graph_name, graph_full in graph_specs.iteritems():
+        print "\nRouting trips on the %s network/timetable: " \
+            % graph_name
+        trip_results = {}
+        for trip_id, trip in sorted(trips_by_id.iteritems()):
+            trip_req_start_dt = trip_req_start_dts[trip_id]
+            res_str = run_trip(server_url, routing_params,
                 trip_req_start_date, trip[2], trip[0],
                 trip[1], otp_router_id=graph_full)
             res = json.loads(res_str)
-            itin = res['plan']['itineraries'][0]
-            print "\nRouting on the %s network/timetable, " \
-                "Requested trip stats:" \
-                % graph_short
-            trip_analysis.print_single_trip_stats(trip[0], trip[1],
-                trip_req_start_dt, itin) 
+            itins = res['plan']['itineraries']
+            if itins:
+                itin_json = res['plan']['itineraries'][0]
+                ti = trip_analysis.TripItinerary(itin_json)
+            else:
+                ti = None
+            trip_results[trip_id] = ti    
+            #print "\nTrip from %s to %s, leaving "\
+            #    "at %s:" % (trip[0], trip[1], trip_req_start_dt)
+            #trip_analysis.print_single_trip_stats(trip[0], trip[1],
+            #    trip_req_start_dt, ti) 
+        trip_results_by_graph[graph_name] = trip_results
+    return trip_results_by_graph
+    
+def save_trip_itineraries(output_base_dir, trip_results_by_graph):
+    if not os.path.exists(output_base_dir):
+        os.makedirs(output_base_dir)
+    for graph_name, trip_results in trip_results_by_graph.iteritems():
+        subdir = os.path.join(output_base_dir, graph_name)
+        if not os.path.exists(subdir):
+            os.makedirs(subdir)
+        for trip_id, trip_itin in trip_results.iteritems():
+            fname = os.path.join(subdir, "%s.json" % trip_id)
+            if trip_itin:
+                trip_itin.save_to_file(fname)
     return
+
+def load_trip_itineraries(output_base_dir, graph_names):
+    trip_results_by_graph = {}
+    for graph_name in graph_names:
+        trip_results = {}
+        subdir = os.path.join(output_base_dir, graph_name)
+        for fname in glob.glob("%s%s*.json" % (subdir, os.sep)):
+            fbase = os.path.basename(fname)
+            trip_id = int(os.path.splitext(fbase)[0])
+            itin = trip_analysis.read_trip_itin_from_file(fname)
+            trip_results[trip_id] = itin
+        trip_results_by_graph[graph_name] = trip_results
+    return trip_results_by_graph
 
 def main():
     SERVER_URL = 'http://130.56.248.56'
@@ -155,11 +169,35 @@ def main():
     dest_lon_lat = (145.089924,-37.717540)
     #origin_lon_lat = (144.876791,-37.749236) 
     #dest_lon_lat = (145.091024,-37.897849)
-    run_single_trip_multi_graphs_print_stats(SERVER_URL, ROUTING_PARAMS,
-        GRAPH_SPECS, trip_req_start_date, trip_req_start_time,
-        origin_lon_lat, dest_lon_lat)
+    #run_single_trip_multi_graphs_print_stats(SERVER_URL, ROUTING_PARAMS,
+    #    GRAPH_SPECS, trip_req_start_date, trip_req_start_time,
+    #    origin_lon_lat, dest_lon_lat)
 
-    #read_then_route_trip_set()
+    #trips_shpfilename = "/Users/Shared/SoftwareDev/UrbanModelling-GIS/OSSTIP/OTP-Routing-Tools/Trips_Generator/output/trips-with_roads_0015-1000.shp"
+    trips_shpfilename = "/Users/Shared/SoftwareDev/UrbanModelling-GIS/OSSTIP/OTP-Routing-Tools/Trips_Generator/output/trips-with_roads_0015-5.shp"
+    trips_set_name = os.path.splitext(os.path.basename(trips_shpfilename))[0]
+    output_base_dir = "./output/%s" % trips_set_name
+    otp_router_srs = osr.SpatialReference()
+    otp_router_srs.ImportFromEPSG(OTP_ROUTER_EPSG)
+    trips_by_id, trips = Trips_Generator.trips_io.read_trips_from_shp_file(
+        trips_shpfilename, otp_router_srs)
+
+    trip_results_by_graph = None
+    route = False
+    load = True
+    analyse = True
+    if route:
+        trip_results_by_graph = route_trip_set_on_graphs(SERVER_URL,
+            ROUTING_PARAMS,
+            GRAPH_SPECS, trip_req_start_date, trips, trips_by_id,
+            output_base_dir)
+        save_trip_itineraries(output_base_dir, trip_results_by_graph)
+    if load:
+        trip_results_by_graph = load_trip_itineraries(output_base_dir,
+            GRAPH_SPECS.keys())
+    if analyse:
+        trip_analysis.calc_print_mean_results(GRAPH_SPECS.keys(),
+            trip_results_by_graph, trips_by_id, trip_req_start_date)
     return
 
 if __name__ == "__main__":
--- TripRunner/trip_analysis.py
+++ TripRunner/trip_analysis.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+import itertools
+import json
 
 import geom_utils
 
@@ -8,43 +10,127 @@ def get_total_sec(td):
 def get_td_pct(td1, td2):
     return get_total_sec(td1) / float(get_total_sec(td2)) * 100
 
-def print_single_trip_stats(origin_lon_lat, dest_lon_lat, trip_req_start_dt, itin):
-    st_raw = itin['startTime']
-    et_raw = itin['endTime']
-    itin_start_dt = datetime.fromtimestamp(st_raw / 1000.0) 
-    itin_end_dt = datetime.fromtimestamp(et_raw / 1000.0)
+class TripItinerary:
+    """This is really a lightweight wrapper class around OTP's 'itinerary'
+    JSON data structure returned from calls to the OTP Planner API. See:-
+    http://docs.opentripplanner.org/apidoc/0.10.0/el_ns0_response.html
+    """
 
-    total_trip_dt = itin_end_dt - trip_req_start_dt
-    total_trip_sec = get_total_sec(total_trip_dt)
-    init_wait_dt = itin_start_dt - trip_req_start_dt    
-    tfer_wait_dt = timedelta(seconds=itin['waitingTime'])
-    total_wait_dt = init_wait_dt + tfer_wait_dt
-    wait_pct = get_td_pct(total_wait_dt, total_trip_dt)
-    walk_dt = timedelta(seconds=itin['walkTime'])
-    walk_pct = get_td_pct(walk_dt, total_trip_dt)
-    transit_dt = timedelta(seconds=itin['transitTime'])
-    transit_pct = get_td_pct(transit_dt, total_trip_dt)
+    def __init__(self, json_data):
+        self.json = json_data
+        # Initialise to empty various results to be cached later.
+        self._dist_travelled = None
+    
+    def get_start_dt(self):
+        st_raw = self.json['startTime']
+        return datetime.fromtimestamp(st_raw / 1000.0) 
 
-    dist_travelled = 0
-    for leg in itin['legs']:
-        dist_travelled += leg['distance']
+    def get_end_dt(self):
+        et_raw = self.json['endTime']
+        return datetime.fromtimestamp(et_raw / 1000.0)
+
+    def get_total_trip_td(self, trip_req_start_dt):
+        return self.get_end_dt() - trip_req_start_dt
+
+    def get_total_trip_sec(self, trip_req_start_dt):
+        return get_total_sec(self.get_total_trip_td(trip_req_start_dt))
+
+    def get_init_wait_td(self, trip_req_start_dt):
+        return self.get_start_dt() - trip_req_start_dt
+
+    def get_tfer_wait_td(self):
+        """I am calling this 'transfer wait' since OTP records in the
+        waitingTime value just time waiting for transfers, not the initial
+        wait."""
+        return timedelta(seconds=self.json['waitingTime'])
+
+    def get_total_wait_td(self, trip_req_start_dt):
+        return self.get_init_wait_td(trip_req_start_dt) \
+            + self.get_tfer_wait_td()
+
+    def get_transit_td(self):
+        return timedelta(seconds=self.json['transitTime'])
+
+    def get_walk_td(self):
+        return timedelta(seconds=self.json['walkTime'])
+
+    def get_dist_travelled(self):
+        """Returns the total trip distance, in m."""
+        if not self._dist_travelled:
+            dist_travelled = 0
+            for leg in self.json['legs']:
+                dist_travelled += leg['distance']
+            self._dist_travelled = dist_travelled
+        return self._dist_travelled
+ 
+    def get_trip_speed_along_route(self, trip_req_start_dt):
+        """Returns the trip speed along route, in km/h"""
+        dist = self.get_dist_travelled()
+        total_trip_sec = self.get_total_trip_sec(trip_req_start_dt)
+        trip_speed_along_route = (dist / 1000.0) \
+            / (total_trip_sec / (60 * 60.0))
+        return trip_speed_along_route
+
+    def save_to_file(self, output_fname):
+        f = open(output_fname, 'w')
+        f.write(json.dumps(self.json))
+        f.close()
+        return
+
+def read_trip_itin_from_file(input_fname):
+    f = open(input_fname, 'r')
+    itin_str = f.read()
+    itin_json = json.loads(itin_str)
+    itin = TripItinerary(itin_json)
+    return itin
+
+########################
+## Analysis and Printing
+
+def get_trip_speed_direct(origin_lon_lat, dest_lon_lat, trip_req_start_dt,
+        trip_itin):
     dist_direct = geom_utils.haversine(origin_lon_lat[0], origin_lon_lat[1],
         dest_lon_lat[0], dest_lon_lat[1])
-
-    trip_speed_along_route = (dist_travelled / 1000.0) \
+    total_trip_sec = trip_itin.get_total_trip_sec(trip_req_start_dt)
+    trip_speed_direct = (dist_direct / 1000.0) \
         / (total_trip_sec / (60 * 60.0))
+    return trip_speed_direct
+
+def print_single_trip_stats(origin_lon_lat, dest_lon_lat, trip_req_start_dt,
+        trip_itin):
+    ti = trip_itin
+    itin_start_dt = ti.get_start_dt()
+    itin_end_dt = ti.get_end_dt()
+    total_trip_td = ti.get_total_trip_td(trip_req_start_dt)
+    total_trip_sec = get_total_sec(total_trip_td)
+
+    init_wait_td = ti.get_init_wait_td(trip_req_start_dt)
+    tfer_wait_td = ti.get_tfer_wait_td()
+    total_wait_td = ti.get_total_wait_td(trip_req_start_dt)
+    walk_td = ti.get_walk_td()
+    transit_td = ti.get_transit_td()
+
+    wait_pct = get_td_pct(total_wait_td, total_trip_td)
+    walk_pct = get_td_pct(walk_td, total_trip_td)
+    transit_pct = get_td_pct(transit_td, total_trip_td)
+
+    dist_travelled = ti.get_dist_travelled()
+    trip_speed_along_route = ti.get_trip_speed_along_route(trip_req_start_dt)
+
+    dist_direct = geom_utils.haversine(origin_lon_lat[0], origin_lon_lat[1],
+        dest_lon_lat[0], dest_lon_lat[1])
     trip_speed_direct = (dist_direct / 1000.0) \
         / (total_trip_sec / (60 * 60.0))
 
     print "Trip departs at %s" % itin_start_dt 
     print "Trip arrives at %s" % itin_end_dt 
-    print "%s total time (inc initial wait)" % total_trip_dt
+    print "%s total time (inc initial wait)" % total_trip_td
     print "  %s (%.2f%%) waiting (%s initial, %s transfers)" \
-        % (total_wait_dt, wait_pct, init_wait_dt, tfer_wait_dt)
+        % (total_wait_td, wait_pct, init_wait_td, tfer_wait_td)
     print "  %s (%.2f%%) walking (for %.2fm)" \
-        % (walk_dt, walk_pct, itin['walkDistance'])
+        % (walk_td, walk_pct, ti.json['walkDistance'])
     print "  %s (%.2f%%) on transit vehicles (%d transfers)" \
-        % (transit_dt, transit_pct, itin['transfers'])
+        % (transit_td, transit_pct, ti.json['transfers'])
     print "Total trip distance (as crow flies): %.2fm." % dist_direct
     print "Total trip distance (travelled): %.2fm." % dist_travelled
     print "(Trip directness ratio:- %.2f)" % (dist_direct / dist_travelled)
@@ -54,4 +140,60 @@ def print_single_trip_stats(origin_lon_lat, dest_lon_lat, trip_req_start_dt, iti
         % trip_speed_direct
     return
 
+def calc_mean_total_time(trip_itins, trip_req_start_dts):
+    sum_val = sum(itertools.imap(
+        lambda trip_id: trip_itins[trip_id].get_total_trip_sec(
+            trip_req_start_dts[trip_id]), trip_itins.iterkeys()))
+    mean_sec = sum_val / float(len(trip_itins))
+    return timedelta(seconds=mean_sec)
 
+def calc_mean_basic_itin_attr(trip_itins, itin_attr):
+    """Convenience function."""
+    sum_val = sum(itertools.imap(
+        lambda ti: ti.json[itin_attr], trip_itins.itervalues()))
+    mean = sum_val / float(len(trip_itins))
+    return mean
+
+def calc_mean_walk_dist(trip_itins):
+    return calc_mean_basic_itin_attr(trip_itins, 'walkDistance')
+
+def calc_mean_transfers(trip_itins):
+    return calc_mean_basic_itin_attr(trip_itins, 'transfers')
+
+def calc_mean_direct_speed(trip_itins, trips_by_id, trip_req_start_dts):
+    sum_val = sum(itertools.imap(
+        lambda trip_id: get_trip_speed_direct(trips_by_id[trip_id][0],
+            trips_by_id[trip_id][1], trip_req_start_dts[trip_id],
+            trip_itins[trip_id]), trip_itins.iterkeys()))
+    mean_spd = sum_val / float(len(trip_itins))
+    return mean_spd
+
+def calc_print_mean_results(graph_names, trip_results_by_graph,
+        trips_by_id, trip_req_start_date):
+    trip_req_start_dts = {}
+    for trip_id, trip in trips_by_id.iteritems():
+        trip_req_start_dts[trip_id] = datetime.combine(trip_req_start_date,
+            trip[2])
+
+    means = {}
+    for graph_name in graph_names:
+        trip_results = trip_results_by_graph[graph_name]
+        means[graph_name] = {}
+        means[graph_name]['total_time'] = \
+            calc_mean_total_time(trip_results, trip_req_start_dts)
+        means[graph_name]['direct_speed'] = \
+            calc_mean_direct_speed(trip_results, trips_by_id,
+                trip_req_start_dts)
+        means[graph_name]['walk_dist'] = calc_mean_walk_dist(trip_results)
+        means[graph_name]['transfers'] = calc_mean_transfers(trip_results)
+
+    print "Mean results were:"
+    for graph_name in graph_names:
+        print "'%s': mean trip time %s, direct speed %.2f km/h, "\
+            "walk dist %.2fm, # of transfers %.1f" % \
+             (graph_name,
+              means[graph_name]['total_time'],
+              means[graph_name]['direct_speed'],
+              means[graph_name]['walk_dist'],
+              means[graph_name]['transfers'])
+    return
