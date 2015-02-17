cc5ea03dd8d1284d821f6b67702194e07b25fcb8
Pat Sunter <patdevelop@gmail.com>
2015-02-17 15:10:08 +1100

    New function read_trips_from_shp_file() to read back trip defs from shape file.

--- Trips_Generator/trips_io.py
+++ Trips_Generator/trips_io.py
@@ -1,5 +1,6 @@
 
 import os, os.path
+from datetime import datetime, time
 from osgeo import ogr, osr
 
 TRIP_LYR_NAME = "trips"
@@ -7,13 +8,15 @@ TRIP_ID_FIELD = "id"
 TRIP_ORIGIN_SLA_FIELD = "orig_sla"
 TRIP_DEST_SLA_FIELD = "dest_sla"
 TRIP_DEP_TIME_FIELD = "dep_time"
+TIME_OUTPUT_STR_FORMAT = "%H:%M"
 
 def save_trips_to_shp_file(filename, trips, trips_srs, output_srs):
     if os.path.exists(filename):
         os.unlink(filename)    
     driver = ogr.GetDriverByName("ESRI Shapefile")
     trips_shp_file = driver.CreateDataSource(filename)
-    trips_lyr = trips_shp_file.CreateLayer(TRIP_LYR_NAME, output_srs, ogr.wkbLineString)
+    trips_lyr = trips_shp_file.CreateLayer(TRIP_LYR_NAME, output_srs,
+        ogr.wkbLineString)
     trips_lyr.CreateField(ogr.FieldDefn(TRIP_ID_FIELD, ogr.OFTInteger))
     field = ogr.FieldDefn(TRIP_ORIGIN_SLA_FIELD, ogr.OFTString)
     field.SetWidth(254)
@@ -42,3 +45,36 @@ def save_trips_to_shp_file(filename, trips, trips_srs, output_srs):
     trips_shp_file.Destroy()
     return
 
+def read_trips_from_shp_file(filename, output_srs):
+    trip_ids_map = {}
+    trips_shp = ogr.Open(filename, 0)
+    if trips_shp is None:
+        print "Error, input trips shape file given, %s , failed to open." \
+            % (filename)
+        sys.exit(1)
+    trips_lyr = trips_shp.GetLayer(0)
+
+    trips_srs = trips_lyr.GetSpatialRef()
+    transform = None
+    if not trips_srs.IsSame(output_srs):
+        transform = osr.CoordinateTransformation(trips_srs, output_srs)
+    trips = []
+    for trip_feat in trips_lyr:
+        trip_id = trip_feat.GetField(TRIP_ID_FIELD)
+        trip_geom = trip_feat.GetGeometryRef()
+        if transform:
+            trip_geom.Transform(transform)
+        trip_origin = trip_geom.GetPoint_2D(0)
+        trip_dest = trip_geom.GetPoint_2D(1)
+        dt = datetime.strptime(
+            trip_feat.GetField(TRIP_DEP_TIME_FIELD), TIME_OUTPUT_STR_FORMAT)
+        trip_dep_time = dt.time()
+        origin_sla = trip_feat.GetField(TRIP_ORIGIN_SLA_FIELD)
+        dest_sla = trip_feat.GetField(TRIP_DEST_SLA_FIELD)
+        trip = (trip_origin, trip_dest, trip_dep_time,
+            origin_sla, dest_sla)
+        trips.append(trip)
+        trip_ids_map[trip_id] = trips[-1]
+    trips_shp.Destroy()
+
+    return trip_ids_map, trips
